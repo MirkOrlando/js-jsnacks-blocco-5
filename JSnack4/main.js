cf03a1ee4f25d4359ffafa774591fb71e1764147
MirkOrlando <mirko@orlando-m.it>
2022-04-12 12:24:34 +0200

    working on JSnack4

---
+++ JSnack4/main.js
@@ -0,0 +1,19 @@
+/* 
+SNACK 4
+Scrivi una funzione che fonda due array 
+(aventi lo stesso numero di elementi) 
+prendendo alternativamente gli elementi da uno e dall’altro
+es. [a,b,c], [1,2,3] → [a,1,b,2,c,3].
+*/
+
+const array1 = ["a", "b", "c"];
+const arrai2 = [1, 2, 3];
+const arraymixed = [];
+array1.forEach((letter) => {
+  arrai2.forEach((number) => {
+    arraymixed.push(letter);
+    arraymixed.push(number);
+  });
+});
+
+console.log(arraymixed);
